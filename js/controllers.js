4306914b4e16b26cac95bb8ef73e3d1cb81ade27
Abraham White <abelincoln.white@gmail.com>
2015-01-29 14:52:02 -0500

    yeah

--- js/controllers.js
+++ js/controllers.js
@@ -11,6 +11,24 @@
     });
 
     appControllers.controller('MembershipCtrl', ['$scope', function($scope){
+       /* $scope.card = new Card({
+            form: 'userForm',
+            container: '.card-wrapper',
+            formSelectors: {
+                numberInput: 'input#payment-number',
+                expiryInput: 'input#payment-exp',
+                cvcInput: 'input#payment-cvc',
+                nameInput: 'input#payment-first, input#payment-last'
+            },
+
+            formatting: true,
+
+            messages: {
+                validDate: 'valid\ndate', // optional - default 'valid\nthru'
+                monthYear: 'mm/yy', // optional - default 'month/year'
+            },
+            debug: true
+        });*/
         $scope.numStudents = 0;
         $scope.data = {
             'accepted': false,
@@ -35,6 +53,18 @@
                     "unit": ""
                 }
             ]
+        };
+        $scope.payment = {
+            "card": {
+                "type":"",
+                "number":"",
+                "exp_month":"",
+                "exp_year":"",
+                "cvv2":"",
+                "first_name":"",
+                "last_name":""
+            },
+            "amount": ""
         };
         $scope.changeStudents = function(num){
             var oldLength = this.user.students.length;
