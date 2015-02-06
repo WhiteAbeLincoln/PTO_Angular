b262c84d133f9788cd8f9270af826e30fdb88f6c
Abraham White <abelincoln.white@gmail.com>
2015-02-06 14:50:34 -0500

    navigation

--- js/app.js
+++ js/app.js
@@ -74,7 +74,42 @@ var myApp = angular.module('myApp', ['ngMaterial', 'ngRoute', 'myApp.controllers
         var self;
 
         return self = {
+            sections: sections,
+            selectSection: function(section){
+                self.openedSection = section;
+            },
+            toggleSelectSection: function(section) {
+                self.openedSection = (self.openedSection === section ? null : section);
+            },
+            isSectionSelected: function(section){
+                return self.openedSection === section;
+            },
 
+            selectPage: function(section, page){
+                page && page.url && $location.path(page.url);
+                self.currentSection = section;
+                self.currentPage = page;
+            },
+            isPageSelected: function(section, page){
+                return self.currentPage === page;
+            }
+        };
+
+        function onLocationChange() {
+            var activated = false;
+            var path = $location.path();
+            sections.forEach(function(section){
+                section.pages.forEach(function(page){
+                  if (path === page.url){
+                      self.selectSection(section);
+                      self.selectPage(section, page);
+                      activated = true;
+                  }
+                });
+            });
+            if (!activated){
+                self.selectSection(sections[3]);
+            }
         }
     }])
 
