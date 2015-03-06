dbdc9ddd394323c6b895905058d50fdecb762009
Abraham White <abelincoln.white@gmail.com>
2015-03-06 15:01:09 -0500

    continued work on download front-end

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,38 @@
 /**
  * Created by abe on 1/7/15.
  */
-(function(){
-var myApp = angular.module('myApp', ['ngMaterial', 'ngRoute', 'myApp.controllers'])
-    .config(['$mdThemingProvider', '$routeProvider', function($mdThemingProvider, $routeProvider){
+(function () {
+    var myApp = angular.module('myApp', ['ngMaterial', 'ngRoute', 'myApp.controllers'])
+        .config(['$mdThemingProvider', '$routeProvider', '$mdIconProvider',
+            function ($mdThemingProvider, $routeProvider, $mdIconProvider) {
 
-        $routeProvider
-            .when('/', {
-                templateUrl: 'partials/home.tmpl.html'
-            })
-            .when('/forms/:tmpl', {
-                templateUrl: function(params){
-					return 'partials/forms/' + params.tmpl + '.tmpl.html'
-                }
+                $routeProvider
+                    .when('/', {
+                        templateUrl: 'partials/home.tmpl.html'
+                    })
+                    .when('/forms/:tmpl', {
+                        templateUrl: function (params) {
+                            return 'partials/forms/' + params.tmpl + '.tmpl.html'
+                        }
 
-            })
-			.when('/downloads', {
-                templateUrl: 'partials/downloads.tmpl.html',
-                controller: 'DownloadCtrl'
-            })
-            .otherwise('/');
+                    })
+                    .when('/downloads', {
+                        templateUrl: 'partials/downloads.tmpl.html',
+                        controller: 'DownloadCtrl'
+                    })
+                    .otherwise('/');
 
-        $mdThemingProvider.theme('default')
-            .primaryPalette('amber')
-            .accentPalette('blue-grey');
-    }]);
+                $mdThemingProvider.theme('default')
+                    .primaryPalette('amber')
+                    .accentPalette('blue-grey');
+
+                $mdIconProvider
+                    .icon('file:document', 'img/icons/file-document-box.svg')
+                    .icon('file:excel', 'img/icons/file-excel-box.svg')
+                    .icon('file:image', 'img/icons/file-image-box.svg')
+                    .icon('file:pdf', 'img/icons/file-pdf-box.svg')
+                    .icon('file:powerpoint', 'img/icons/file-powerpoint-box.svg')
+                    .icon('file:word', 'img/icons/file-word-box.svg')
+                    .defaultIconSize('48px,48px');
+            }]);
 })();
--- public/js/controllers/DownloadCtrl.js
+++ public/js/controllers/DownloadCtrl.js
@@ -4,14 +4,44 @@
 (function() {
     angular.module('myApp.controllers')
         .controller('DownloadCtrl', ['$scope', 'Download', function ($scope, Download) {
+            $scope.iconStyles = {
+                pdf: {
+                    color: '#DB4437',
+                    height: '48px',
+                    width: '48px'
+                },
+                document: {
+                    color: '#4285f4',
+                    height: '48px',
+                    width: '48px'
+                },
+                excel: {
+                    color: '#007133',
+                    height: '48px',
+                    width: '48px'
+                },
+                image: {
+                    color: 'SkyBlue',
+                    height: '48px',
+                    width: '48px'
+                },
+                powerpoint: {
+                    color: '#DB5A26',
+                    height: '48px',
+                    width: '48px'
+                },
+                word: {
+                    color: '#2A3281',
+                    height: '48px',
+                    width: '48px'
+                }
+            };
+
             $scope.dltypes = [
                 {
                     name:"Meeting Notes",
                     downloads: [
                         {
-                            get fileIcon() {
-                                return "img/icons/file-"+this.fileType+"-box.svg"
-                            },
                             fileType:"pdf",
                             name:"Meeting notes 12/12",
                             shortDesc:"Created a new download page and threw a party",
@@ -20,10 +50,7 @@
                             id:"12"
                         },
                         {
-                            get fileIcon() {
-                                return "img/icons/file-"+this.fileType+"-box.svg"
-                            },
-                            fileType:"pdf",
+                            fileType:"word",
                             name:"Meeting notes 12/14",
                             shortDesc:"Created a new download page and threw a party",
                             description:"long description",
@@ -31,10 +58,23 @@
                             id:"14"
                         },
                         {
-                            get fileIcon() {
-                                return "img/icons/file-"+this.fileType+"-box.svg"
-                            },
                             fileType:"document",
+                            name:"Meeting notes 11/14",
+                            shortDesc:"Created a new download page and threw a party",
+                            description:"long description",
+                            modDate:"2012-11-01 15:30:33",
+                            id:"17"
+                        },
+                        {
+                            fileType:"excel",
+                            name:"Meeting notes 11/14",
+                            shortDesc:"Created a new download page and threw a party",
+                            description:"long description",
+                            modDate:"2012-11-01 15:30:33",
+                            id:"19"
+                        },
+                        {
+                            fileType:"powerpoint",
                             name:"Meeting notes 11/14",
                             shortDesc:"Created a new download page and threw a party",
                             description:"long description",
@@ -48,9 +88,6 @@
                     name:"Pictures",
                     downloads: [
                         {
-                            get fileIcon() {
-                                return "img/icons/file-"+this.fileType+"-box.svg"
-                            },
                             fileType:"image",
                             name:"Birthday Cake",
                             shortDesc:"Created a new download page and threw a party",
@@ -59,9 +96,66 @@
                             id:"13"
                         },
                         {
-                            get fileIcon() {
-                                return "img/icons/file-"+this.fileType+"-box.svg"
-                            },
+                            fileType:"image",
+                            name:"New Dog",
+                            shortDesc:"I got a dog!",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"15"
+                        },{
+                            fileType:"image",
+                            name:"Birthday Cake",
+                            shortDesc:"Created a new download page and threw a party",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"13"
+                        },
+                        {
+                            fileType:"image",
+                            name:"New Dog",
+                            shortDesc:"I got a dog!",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"15"
+                        },{
+                            fileType:"image",
+                            name:"Birthday Cake",
+                            shortDesc:"Created a new download page and threw a party",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"13"
+                        },
+                        {
+                            fileType:"image",
+                            name:"New Dog",
+                            shortDesc:"I got a dog!",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"15"
+                        },{
+                            fileType:"image",
+                            name:"Birthday Cake",
+                            shortDesc:"Created a new download page and threw a party",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"13"
+                        },
+                        {
+                            fileType:"image",
+                            name:"New Dog",
+                            shortDesc:"I got a dog!",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"15"
+                        },{
+                            fileType:"image",
+                            name:"Birthday Cake",
+                            shortDesc:"Created a new download page and threw a party",
+                            description:"long description",
+                            modDate:"2012-12-01 15:30:33",
+                            id:"13"
+                        },
+                        {
                             fileType:"image",
                             name:"New Dog",
                             shortDesc:"I got a dog!",
