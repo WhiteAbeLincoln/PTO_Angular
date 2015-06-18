89cb5691aede8708184a2171bf08a3f1aa312209
Abraham White <abelincoln.white@gmail.com>
2015-06-17 22:36:27 -0400

    Update www.sh

--- www.sh
+++ www.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-DEBUG=pto:* ./bin/www;
+export PTOMYSQL_IP='127.0.0.1'
+export PTOMYSQL_PORT='3306'
+export PTOMYSQL_USER='root'
+export PTOMYSQL_PASSWORD='CHANGE_THIS_PASSWORD'
+./bin/www;
